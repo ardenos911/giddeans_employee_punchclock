dc9a5f932966cf42ced30267962fb994c7585b97
ardenos911 <ardenos@protonmail.com>
2023-09-07 05:01:32 -0400

    some more login UI changes

--- employee_attendance/lib/screens/login_screen.dart
+++ employee_attendance/lib/screens/login_screen.dart
@@ -9,6 +9,8 @@ class LoginScreen extends StatefulWidget {
 }
 
   class _LoginScreenState extends State<LoginScreen> {
+    final TextEditingController _emailController = TextEditingController();
+
     @override
     Widget build(BuildContext context) {
        double screenWidth = MediaQuery.of(context).size.width;
@@ -22,7 +24,7 @@ class LoginScreen extends StatefulWidget {
                height: screenHeight/3,
                width: screenWidth,
                decoration: const BoxDecoration(
-                 color: Colors.redAccent,
+                 color: Color.fromARGB(255, 1, 32, 58),
                  borderRadius: BorderRadius.only(
                   bottomRight: Radius.circular(70),
                   bottomLeft: Radius.circular(70)
@@ -35,7 +37,7 @@ class LoginScreen extends StatefulWidget {
                     SizedBox(height: 20,),
                     Text('Giddens TimeKeeper App',style:TextStyle(
                       fontSize:25,
-                      color: Colors.white70,
+                      color: Color.fromARGB(179, 190, 242, 2),
                       fontWeight:FontWeight.bold
                     )),
                   ],
